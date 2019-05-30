af23ca000a3bc9dc3f2bc3e9220194c8a5a7cdb6
lpurgitoryl <Ngarcia201@gmail.com>
2019-05-30 13:54:27 -0700

    Finsihed sorting algos. Only thing is that im not sure I impelemented them correctly. For future refrence, check if the algos actually was able to sort the arrays by writting the conents to a file before and after "sorting".

--- Lab9/main.cpp
+++ Lab9/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <algorithm>
 
 using namespace std;
 
@@ -33,13 +34,71 @@ int main (){
     clock_t End = clock();
     int elapsedTime = (End - Start)/CLOCKS_PER_MS; // converts elapsed time from microseconds to milliseconds.
     
-    cout << "It took " << elapsedTime << " for the Quick_Sort_midpoint on set1 " << endl; 
+    cout << "It took " << elapsedTime << "ms for the Quick_Sort_midpoint on set1 " << endl; 
+    Start = clock();
+    //call sort function here
+    Quicksort_midpoint(set1, 0, NUMBERS_SIZE - 1);
+    End = clock();
+    elapsedTime = (End - Start)/CLOCKS_PER_MS; // converts elapsed time from microseconds to milliseconds.
+    
+    cout << "It took " << elapsedTime << "ms for the Quick_Sort_meadian on set1 (sorted) " << endl; 
+    cout << endl << endl << endl;
+///////
+    Start = clock();
+    //call sort function here
+    Quicksort_medianOfThree(set2, 0, NUMBERS_SIZE - 1);
+    End = clock();
+    elapsedTime = (End - Start)/CLOCKS_PER_MS; // converts elapsed time from microseconds to milliseconds.
+    
+    cout << "It took " << elapsedTime << "ms for the Quick_Sort_meadian on set2 " << endl; 
+
+     Start = clock();
+    //call sort function here
+    Quicksort_medianOfThree(set2, 0, NUMBERS_SIZE - 1);
+    End = clock();
+    elapsedTime = (End - Start)/CLOCKS_PER_MS; // converts elapsed time from microseconds to milliseconds.
+    
+    cout << "It took " << elapsedTime << "ms for the Quick_Sort_meadian on set2 (sorted)" << endl; 
+    cout << endl << endl << endl;
+//
+    Start = clock();
+    //call sort function here
+    InsertionSort(set3, NUMBERS_SIZE );
+    End = clock();
+    elapsedTime = (End - Start)/CLOCKS_PER_MS; // converts elapsed time from microseconds to milliseconds.
+    
+    cout << "It took " << elapsedTime << "ms for the Insertion Sort on set3 " << endl; 
+
+    Start = clock();
+    //call sort function here
+    InsertionSort(set3, NUMBERS_SIZE );
+    End = clock();
+    elapsedTime = (End - Start)/CLOCKS_PER_MS; // converts elapsed time from microseconds to milliseconds.
+    
+    cout << "It took " << elapsedTime << "ms for the Insertion Sort on set3(sorted) " << endl; 
 
 
 return 0;
 
 }
+void InsertionSort(int numbers[], int numbersSize){
+    int i = 0;
+    int j = 0;
+    int temp = 0;
+    for ( i = 0 ; i < numbersSize ; ++i){
+        j = i;
 
+        while ( j > 0 && numbers[j] < numbers[j - 1] ){
+            //swap
+            temp = numbers[j];
+            numbers[j] = numbers[j - 1];
+            numbers[j - 1] = temp;
+
+            --j;
+        }
+    }
+
+}
 int Partition (int arr[], int i, int k){
     int l = 0;
     int h = 0;
@@ -96,4 +155,70 @@ void Quicksort_midpoint(int numbers[], int i, int k){
    Quicksort_midpoint(numbers, j + 1, k );
 }
 
+int Partition_median(int arr[], int i, int k){
+    int l = 0;
+    int h = 0;
+    int midpoint = 0;
+    int pivot = 0;
+    int temp = 0;
+    bool done = false;
 
+    //calculates middles element
+    // i is starting index of arr, k is last index of the arr 
+    midpoint = i + ( k - i) / 2 ;
+    if( ( arr[i] >= arr[k] && arr[i] <= arr[midpoint] ) || ( arr[i] >= arr[midpoint] && arr[i] <= arr[k] ) ){
+         pivot = arr[i];
+    }
+    else if(( arr[midpoint] >= arr[i] && arr[midpoint] <= arr[k] ) || ( arr[midpoint] >= arr[k] && arr[midpoint] <= arr[i] ) ){
+        pivot = arr[midpoint];
+    }
+    else
+    {
+        pivot = arr[k];
+    }
+    
+    
+
+    l = i;
+    h = k;
+
+    while (!done){
+        while (arr [l] < pivot){
+            ++l;
+        }
+
+        while (pivot < arr[h]){
+            --h;
+        }
+
+        if ( l >= h){
+            done = true;
+        }
+
+        else {
+            temp = arr[l];
+            arr[l] = arr[h];
+            arr[h] = temp;
+
+            ++l;
+            --h;
+        }
+       
+    }
+
+     return h;
+}
+
+
+void Quicksort_medianOfThree(int numbers[], int i, int k){
+    int j = 0;
+
+    if ( i >= k){
+        return;
+    } 
+
+    j = Partition_median(numbers, i, k);
+
+    Quicksort_medianOfThree(numbers, i , j);
+    Quicksort_medianOfThree(numbers, j + 1, k );
+}
